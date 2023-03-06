db0ff66f19471263684d0020146c5478c6e03b56
FroztGal <froztgal@gmail.com>
2023-03-06 15:40:08 +0300

    test async

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import asyncio
 import logging
+import sys
 
 from flask import Flask
 
 from bot import main
 
 app = Flask(__name__)
-loop = asyncio.get_event_loop()
+if sys.platform == "win32" and sys.version_info >= (3, 8, 0):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 logging.basicConfig(level=logging.INFO)
 
