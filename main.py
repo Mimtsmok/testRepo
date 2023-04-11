91973dbfee990906e103dcd07fb396b3f2306875
Puz <froztgal@gmail.com>
2023-04-11 12:53:10 +0300

    test

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import asyncio
 import logging
 import sys
+from threading import Thread
 
 from flask import Flask
 
 from bot import main
 
 app = Flask(__name__)
-if sys.platform == "win32" and sys.version_info >= (3, 8, 0):
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 logging.basicConfig(level=logging.INFO)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
     return "Hello"
 
-@app.route('/start')
-async def start():
-    await main()
-    return "OK"
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+class FlaskThread(Thread):
+    def run(self) -> None:
+        app.run(host="0.0.0.0", port=80)
+
+
+class TelegramThread(Thread):
+    async def run(self) -> None:
+        await main()
+
+
+if __name__ == "__main__":
+    flask_thread = FlaskThread()
+    flask_thread.start()
+
+    main()
